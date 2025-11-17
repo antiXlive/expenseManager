6823a68adf6f5746e42e4d0ffb4a7dc7693bdee5
Piyush Kumar <piyush107iiitm@gmail.com>
2025-11-17 21:53:59 +0530

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,26 @@
+const CACHE = "expense-cache-v1";
+
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./service-worker.js",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png"
+];
+
+// Install
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE).then(cache => cache.addAll(ASSETS))
+  );
+});
+
+// Fetch – offline first
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(resp => {
+      return resp || fetch(event.request);
+    })
+  );
+});
